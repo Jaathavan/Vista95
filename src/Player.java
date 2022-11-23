adb871a63513e7fe9a1f7c3a183059d7ef60dbea
Jaathavan <rjaathu@outlook.com>
2022-11-22 21:53:11 -0500

    basic left and right + ideal state

---
+++ src/Player.java
@@ -0,0 +1,97 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.*;
+import java.io.*;
+
+public class Player {
+    private int x,y;
+    private int xa = 1;
+    private int ya = 0;
+
+    private boolean left = false, right = false, space = false;
+
+    private BufferedImage player = null;
+
+    public Player() {
+        this.x = 200;
+        this.y = 200;
+        try {
+            player = ImageIO.read(new File("resources/Balmer/ideal_1.png"));
+        } catch (IOException e)
+        {
+            System.out.println("No Image");
+        }
+    }
+    
+    public BufferedImage getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(String src) {
+        try {
+            player = ImageIO.read(new File("resources/Balmer/" + src));
+        } catch (IOException e)
+        {
+            System.out.println("No Image");
+        }
+    }
+
+    public void keyPressed(KeyEvent e) {
+		// This checks to see which key was pressed, and then sets the appropriate
+		// Boolean to true
+		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+			left = true;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			right = true;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+			space = true;
+		}
+	}
+
+	public void keyReleased(KeyEvent e) {
+		// When the key is released, set the Boolean to false, and change
+		// acceleration to 0
+		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+			left = false;
+			xa = 0;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			right = false;
+			xa = 0;
+		}
+
+		if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+			space = false;
+		}
+	}
+
+    public void move() {
+		// Base your acceleration on the Booleans set by your keyPressed method
+		if (right) {
+			xa = 2;
+		}
+		if (left) {
+			xa = -2;
+		}
+
+		x += xa;
+		y += ya;
+	}
+
+    public void paint(Graphics g) {
+        if (right) {
+            setPlayer("walking_1.png");
+            g.drawImage(player, x, y, null);
+        }
+        if (left) {
+            setPlayer("walking_1.png");
+            g.drawImage(player, x + player.getWidth(), y, -player.getWidth(), player.getHeight(), null);
+        } else {
+            setPlayer("ideal_1.png");
+            g.drawImage(player, x, y, null);
+        }       
+    }
+}
---
+++ src/Vista95.java
@@ -0,0 +1,58 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+public class Vista95 extends JPanel {
+    private Player p = new Player();
+	
+	public Vista95() {
+
+		addKeyListener(new KeyListener() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				// Passes the KeyEvent e to the ball instance
+				p.keyReleased(e);
+			}
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+				// Passes the KeyEvent e to the ball instance
+				p.keyPressed(e);
+			}
+		});
+
+		setFocusable(true);
+	}
+
+	public void move() {
+		p.move();
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		
+		p.paint(g2d);
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		JFrame frame = new JFrame("Vista 95");
+		Vista95 v95 = new Vista95();
+		frame.add(v95);
+		frame.setSize(1020, 640);
+		frame.setVisible(true);
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		while (true) {
+			v95.move();
+			v95.repaint();
+			Thread.sleep(10);
+		}
+	}
+}
